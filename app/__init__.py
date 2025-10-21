59eb2d081331062ec76ba5167ad2e4751bfcf318
Tudor <tudor204@hotmail.com>
2025-10-21 16:49:41 +0200

    Primer commit

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask
+
+
+
+load_dotenv()
+
+
+app = Flask(__name__)
+app.secret_key = os.getenv('FLASK_SECRET_KEY')
+
+from app.controllers import (
+    IndexController
+)
